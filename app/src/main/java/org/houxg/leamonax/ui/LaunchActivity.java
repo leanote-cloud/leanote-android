e1d3df306382265c6462d53966122b3e7672047e
bin <bin6160@126.com>
2017-03-29 14:01:30 +0800

    add launch background

--- app/src/main/java/org/houxg/leamonax/ui/LaunchActivity.java
+++ app/src/main/java/org/houxg/leamonax/ui/LaunchActivity.java
@@ -3,7 +3,11 @@ package org.houxg.leamonax.ui;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
 
+import org.houxg.leamonax.R;
 import org.houxg.leamonax.model.Account;
 import org.houxg.leamonax.network.ApiProvider;
 import org.houxg.leamonax.service.AccountService;
@@ -13,7 +17,10 @@ public class LaunchActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent;
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_launch);
+        final Intent intent;
         if (AccountService.isSignedIn()) {
             Account account = Account.getCurrent();
             ApiProvider.getInstance().init(account.getHost());
@@ -22,7 +29,12 @@ public class LaunchActivity extends Activity {
             intent = new Intent(this, SignInActivity.class);
         }
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(intent);
-        finish();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
     }
 }
